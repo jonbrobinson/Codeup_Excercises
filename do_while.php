0323cc275551850885399abcdabb622a4595b52e
jonathan robinson <jonbrobinson@gmail.com>
2014-05-10 12:28:48 -0500

    Added mew multiplication  function to produce the same result

--- do_while.php
+++ do_while.php
@@ -1,12 +1,13 @@
 <?php
 
-//Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a 
-//is less than 1,000,000. Output should equal
+// Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a 
+// is less than 1,000,000. Output should equal
+
 $a = 2;
 
 do {
 	echo $a . PHP_EOL;
-	$a = $a*$a;
+	$a *= $a;
 } while ($a < 1000000);
 
 ?>
