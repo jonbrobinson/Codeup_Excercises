46d313a3a0530709c093814eb91cb6f34fdabd8e
jonathan robinson <jonbrobinson@gmail.com>
2014-05-10 12:10:27 -0500

    Added new function to Produce Var * Var result

--- do_while.php
+++ do_while.php
@@ -1,10 +1,12 @@
 <?php
 
-$a = 100;
+//Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a 
+//is less than 1,000,000. Output should equal
+$a = 2;
 
 do {
 	echo $a . PHP_EOL;
-	$a -= 5;
-} while ($a >= -10);
+	$a = $a*$a;
+} while ($a < 1000000);
 
 ?>
