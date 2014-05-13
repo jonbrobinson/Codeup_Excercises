ba174414692b7c8dacd48a60658fc77262744bd7
jonathan robinson <jonbrobinson@gmail.com>
2014-05-13 11:04:33 -0500

    Added fwrite notes on command line

---
+++ cli_sapi.php
@@ -0,0 +1,11 @@
+<?php
+
+// Write the output
+// Notice the space after the ?
+fwrite(STDOUT, 'What is your first name? ');
+
+// Get the input from user
+$first_name = fgets(STDIN);
+
+// Output the user's name
+fwrite(STDOUT, "Hello $first_name\n");
