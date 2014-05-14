b3eeb4a49f2068d1f73f43bfabbefcf9c3a0c7aa
jonathan robinson <jonbrobinson@gmail.com>
2014-05-14 11:23:46 -0500

    created function to break at 10

--- break_continue.php
+++ break_continue.php
@@ -1,11 +1,11 @@
 <?php
 
-// Create a for loop that shows all even numbers between 1 and 100 
-// using continue
+// Create another for loop that counts from 1 to 100, 
+// but stops after 10 using break
 
 for ($i = 1; $i <= 100; $i++){
-	if ($i % 2 == 0){
-		echo $i . PHP_EOL;
-		continue;
+	echo $i . PHP_EOL;
+	if ($i == 10){
+		break;
 	}
 }
