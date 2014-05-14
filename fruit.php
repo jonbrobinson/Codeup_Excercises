b31f70c77140bae151ac68ed1451c1260aa8cbc3
jonathan robinson <jonbrobinson@gmail.com>
2014-05-13 21:10:17 -0500

    Added foreach loop for  Array

---
+++ fruit.php
@@ -0,0 +1,11 @@
+<?php
+
+$fruits = array('apple', 'banana', 'strawberry', 'orange', 'cherry');
+
+// for($i = $fruits ; $i <= 4 ; $i += 1){ 
+// 	echo ($i . PHP_EOL); 
+// }
+
+foreach($fruits as $fruit){
+	echo $fruit . PHP_EOL;
+}
