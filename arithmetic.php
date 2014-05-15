1e29d75dbb04907f4e31d193d523a642ff0c0ce6
jonathan robinson <jonbrobinson@gmail.com>
2014-05-15 09:58:10 -0500

    Created file arithmetic to demonstate basic of function creation

---
+++ arithmetic.php
@@ -0,0 +1,30 @@
+<?php
+
+function add($a, $b){
+	echo $a + $b . PHP_EOL;
+}
+
+add(10,2);
+
+function subtract($a, $b){
+	echo $a - $b . PHP_EOL;
+}
+subtract(10,2);
+
+function multiply($a, $b){
+	 echo $a * $b . PHP_EOL;
+}
+
+multiply(10,2);
+
+function divide($a, $b){
+	echo $a / $b . PHP_EOL;
+}
+
+divide(10,2);
+
+function modulus($a, $b){
+	echo $a % $b . PHP_EOL;
+}
+
+modulus(10,2);
