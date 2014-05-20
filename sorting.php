115cf8ae853eeddd8c6d9257fae61a58973659f9
jonathan robinson <jonbrobinson@gmail.com>
2014-05-20 14:13:03 -0500

    Added file sorting to demonstrate sort functions with arrays

---
+++ sorting.php
@@ -0,0 +1,20 @@
+<?php
+
+$princesses = array(
+	'Princess Bublegum',
+	'FLame Princess',
+	'Lumpy Space Princess',
+	'Hot Dog Princess',
+	'Slime Princess'
+);
+
+$ghost_index = array_search('Ghost Princesses', $princesses);
+$pb_index = array
+
+$users = array(
+	'stretch' => 'Jake the Dog',
+	'heroboy' => 'Finn the Human'
+	'sPetrikov' => 'The Ice King'
+	'hambo' => 'Marceline'
+)
+
